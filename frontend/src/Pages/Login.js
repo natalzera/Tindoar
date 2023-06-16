7c88c190769a7074b307cb2a26b921f9d166dfd6
natalzera <gabnatal@gmail.com>
2023-06-15 21:05:42 -0300

    login iniciado

--- frontend/src/Pages/Login.js
+++ frontend/src/Pages/Login.js
@@ -6,12 +6,12 @@ const Login = () => {
         <div className='content-login'>
             <div className='content-title-login'>
                 <img src='./img/logo.jpg' alt='logo tindoar' className='img-logo-login'/>
-                <h1>Tindoar</h1>
+                <h1>tindoar</h1>
             </div>
 
             <div className='types-user-list'>
-                <div className='type-user'>Doador</div>
-                <div className='type-user'>Entidade</div>
+                <p className='type-user current-type'>Doador</p>
+                <p className='type-user'>Entidade</p>
             </div>
 
             <div className='forms-login'>
@@ -23,7 +23,7 @@ const Login = () => {
                     <p>Senha</p>
                     <input type='password' id='input-password'></input>
                 </div>
-                <input type='button' value='Login' id='button-login'></input>
+                <input type='button' value='Login' className='button-login' id='button-login'></input>
             </div>
 
             <p id='create-acc'>Criar uma conta</p>
